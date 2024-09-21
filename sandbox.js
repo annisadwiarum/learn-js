b2b5a6c1a30d9807191ae5e1cf3737be3f6282b7
annisadwiarum <annisadwiarum710@gmail.com>
2024-09-21 16:32:34 +0700

    syntax basics & types

--- sandbox.js
+++ sandbox.js
@@ -12,3 +12,40 @@ console.log(points);
 
 var score = 50;
 console.log(score);
+
+// tes string
+console.log("hello world, merhaba");
+
+let email = "annisadwiarum710@gmail.com";
+console.log(email);
+
+// string concatenation
+let firstName = "Annisa";
+let lastName = "Dwi Arum";
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+console.log(fullName[1]);
+console.log(fullName[2]);
+
+//string length
+console.log(fullName.length);
+
+// string method
+console.log(fullName.toLowerCase());
+
+// let result = fullName.toUpperCase();
+// console.log(result);
+
+let index = email.indexOf('@');
+console.log(index);
+
+//string method
+// let result = email.lastIndexOf('a');
+let results = email.slice(0, 6);
+let result = email.substr(4, 6);
+console.log(results);
+console.log(result);
