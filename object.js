010e23cf405a2da5225df7bc14ae32cb2385aeed
annisadwiarum <annisadwiarum710@gmail.com>
2024-10-08 17:01:14 +0700

    objects

---
+++ object.js
@@ -0,0 +1,5 @@
+// object itu bagian oaling besar di javascript. inti dari banyak hal yang bisa kita lakukan
+// ia memilikii hal-hal yang dapat mereka lakukan dengan benar.
+// contoh nyata dari sebuah objek adalah ponsel. ponsel adalah objek nyata dan memiliki properti
+// properti tersebut bisa berupa warna, ukuran, dll.
+// ponsel juga bisa memiliki hal-hal yang dapat di lakukan, seperti berdering, mengambil gambar, dll.
